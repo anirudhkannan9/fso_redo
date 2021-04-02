8dd1fa164630ad75c81f0b1fdb4d9ebb9eb0ba94
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-04-01 22:59:41 -0700

    first commit - trying again - ex1.3 done

---
+++ submissions/part1/courseinfo/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} />
+      <Part part={props.part2}/>
+      <Part part={props.part3}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}
+    </p>
+  )
+
+}
+
+const App = () => {
+  //just a comment
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React', 
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data', 
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total part1={part1} part2={part2} part3={part3}/>
+    </div>
+  )
+  
+}
+
+export default App
