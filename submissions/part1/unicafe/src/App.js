29cc9ae2017a9aa70eff229590a4b492a96f3a6a
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-04-04 23:27:16 -0700

    Ex 1.10: unicafe step5

--- submissions/part1/unicafe/src/App.js
+++ submissions/part1/unicafe/src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
 
 const Button = ({operation, text}) => <button onClick={operation}>{text}</button>
-const Statistic = ({param, value}) => <div>{param} {value}</div>
+const Statistic = ({text, value}) => <div>{text} {value}</div>
   
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -37,12 +36,12 @@ const App = () => {
       <Button operation={incrementBad} text="bad"/>
 
       <h1>statistics</h1>
-      <Statistic param="good" value={good}/>
-      <Statistic param="neutral" value={neutral}/>
-      <Statistic param="bad" value={bad}/>
-      <Statistic param="all" value={good+bad+neutral}/>
-      <Statistic param="average" value={(good - bad)/(good+bad+neutral)}/>
-      <Statistic param="positive (%)" value={(good/(good+neutral+bad))*100}/>
+      <Statistic text="good" value={good}/>
+      <Statistic text="neutral" value={neutral}/>
+      <Statistic text="bad" value={bad}/>
+      <Statistic text="all" value={good+bad+neutral}/>
+      <Statistic text="average" value={(good - bad)/(good+bad+neutral)}/>
+      <Statistic text="positive (%)" value={(good/(good+neutral+bad))*100}/>
 
     </div>
   )
