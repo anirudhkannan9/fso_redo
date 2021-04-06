3ef5c1ca94a81c7b1d883e9b104b8ed628c00aed
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-04-05 23:30:57 -0700

    Ex 1.12*

---
+++ submissions/part1/anecdotes/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const selectRandom = () => {
+    setSelected((Math.floor(Math.random() * (5 - 0 +1)) + 0))
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]} 
+      <br></br>
+      <button onClick={selectRandom}> next anecdote </button>
+    </div>
+  )
+}
+
+export default App
