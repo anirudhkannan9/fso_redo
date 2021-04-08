613e3663a54aa0fe650cdca345acef1c51994fe5
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-04-07 22:54:55 -0700

    Ex 2.1: course information step 6

---
+++ submissions/part2/courseinfo/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => <h1>{course.name}</h1>
+  
+// const Total = ({ course }) => {
+//   const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+//   return(
+//     <p>Number of exercises {sum}</p>
+//   ) 
+// }
+
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>    
+
+const Content = ({ course }) => <div> {course.parts.map(part => <Part key={part.id} part={part} />)}</div>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = ({course}) => {  
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course}/> 
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
