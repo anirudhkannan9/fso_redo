9dfd2d07ab7e390d7dc50bf68f68a3f3ee446f4e
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-04-09 23:06:04 -0700

    Ex 2.9 phonebook step 4

--- submissions/part2/phonebook/src/App.js
+++ submissions/part2/phonebook/src/App.js
@@ -1,9 +1,28 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchString, setSearchString ] = useState('')
+  const [ showAll, setShowAll ] = useState(true)
+
+
+  
+  const handleSearchStringChange = (event) => {
+    setSearchString(event.target.value)
+    setShowAll(false)
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -21,13 +40,20 @@ const App = () => {
     }
   }
 
-  const handleNameChange = (event) => setNewName(event.target.value)
-  
-  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const peopleToShow = showAll
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(searchString.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input
+                            value={searchString}
+                            onChange={handleSearchStringChange}
+                            />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -46,7 +72,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {peopleToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
